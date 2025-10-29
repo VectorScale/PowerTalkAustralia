7b3aa503c6758d5ecb852928d822e5d7334d11bd
Cody Schaenzel <32047327+VectorScale@users.noreply.github.com>
2025-10-29 15:28:15 +1000

    Update attendance.js to fix DELETE into POST

--- backend/routes/attendance.js
+++ backend/routes/attendance.js
@@ -39,7 +39,7 @@ router.post("/join", (req, res) => {
  * @returns {Object} Success message or database error  
  * @throws {500} Database operation failure
  */
-router.delete("/notjoin", (req, res) => {
+router.post("/notjoin", (req, res) => {
    let { user_id, meeting_id }= req.body;
   const query = "DELETE FROM meeting_attendance WHERE user_id = ? AND meeting_id = ?";
   db.query(query, [ user_id , meeting_id] , (err, result) => {
